831da4f5fc9ba1049b9d2890715e070668362a4b
Mariana Pillon <87533378+m-pillon@users.noreply.github.com>
2025-05-01 14:43:21 -0300

    Update settings.py

--- SleepScale/settings.py
+++ SleepScale/settings.py
@@ -17,8 +17,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
